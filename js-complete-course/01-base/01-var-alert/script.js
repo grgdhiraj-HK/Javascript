3ebba00cf89d2849ce9ec0aaf2f9561bf95970a0
DHIRAJ_GURUNG <grgdhiraj.be@gmail.com>
2019-12-16 12:14:30 +0100

    Completed 01-var-alert

--- js-complete-course/01-base/01-var-alert/script.js
+++ js-complete-course/01-base/01-var-alert/script.js
@@ -10,8 +10,8 @@
 // You will have time to focus on it later.
 
 (function() {
-    let whatever = "hello world!!!";
-    alert(whatever);
+    let greetings = "Hello, World!";
+    alert(greetings);
     // your code here
 
 })();
