7ed97b290b635449f3f9fd0c6f2e1ced85067e94
DHIRAJ_GURUNG <grgdhiraj.be@gmail.com>
2019-12-16 17:12:29 +0100

    Completed 04-asv

--- js-complete-course/01-base/04-asv-confirm/script.js
+++ js-complete-course/01-base/04-asv-confirm/script.js
@@ -10,7 +10,16 @@
 // You will have time to focus on it later.
 
 (function() {
+    let name = prompt('What is your name?');
+    let age = prompt('How old are you?');
+    let town = prompt("Where do you live?");
 
+    alert(`Your name is ${name} and you are ${age} years old. Your Town is ${town}`);
+    if (confirm(`Are you  ${name} aged  ${age} years old from ${town}?`)) {
+    } else {
+
+        // Do nothing!
+    }
     // your code here
 
 })();
