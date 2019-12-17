969965a0dfee100c1dd3d5d58b791b7c0165c28b
DHIRAJ_GURUNG <grgdhiraj.be@gmail.com>
2019-12-17 16:10:58 +0100

    bcg-one done with HEX code

--- js-complete-course/03-colors/01-change-bcg-one/script.js
+++ js-complete-course/03-colors/01-change-bcg-one/script.js
@@ -1,3 +1,4 @@
+
 /* becode/javascript
  *
  * /03-colors/01-change-bcg-one/script.js - 3.1: couleur de fond (1)
@@ -10,6 +11,23 @@
 // You will have time to focus on it later.
 
 (function() {
+    //added on click for each button
+    document.getElementById("red").addEventListener("click", function() {
+        // perform an addition
+        document.body.style.backgroundColor = "#ef0a0a";
+    });
+    document.getElementById("green").addEventListener("click", function() {
+        // perform an addition
+        document.body.style.backgroundColor = "#17dc0a";
+    });
+    document.getElementById("yellow").addEventListener("click", function() {
+        // perform an addition
+        document.body.style.backgroundColor = "#f3f30e";
+    });
+    document.getElementById("blue").addEventListener("click", function() {
+        // perform an addition
+        document.body.style.backgroundColor = "#0b5a9c";
+    });
 
     // your code here
 
