b38ecf7a10b71173d1bf8a28a9cb7db8653e1b85
Dhiraj Gurung <57953730+grgdhiraj@users.noreply.github.com>
2019-12-28 20:52:24 +0100

    06-12 done!

--- js-complete-course/06-dom/12-change-event-input-two/script.js
+++ js-complete-course/06-dom/12-change-event-input-two/script.js
@@ -10,7 +10,24 @@
 // You will have time to focus on it later.
 
 (function() {
-
     // your code here
+
+    document.getElementById("pass-one").addEventListener("input", function () {
+   let pwdElement = document.getElementById("pass-one");
+   let pwdValue = pwdElement.value;
+   let pwdLength = pwdValue.length;
+   let pwdNumberValue = pwdValue.match(/\d/g);
+
+   if (pwdLength >=8 && pwdNumberValue!=null && pwdNumberValue.length >=2){
+        //&& is to do two conditions together instead of multiple 'if'
+           document.getElementById("validity").innerHTML="Ok";
+   }
+    else {
+       document.getElementById("validity").innerHTML="Not ok";
+   }
+    //Learn more about RegExp!
+
+    })
+
 
 })();
