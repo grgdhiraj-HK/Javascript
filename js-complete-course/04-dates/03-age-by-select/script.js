befc6eea646f9155c08049ffbe8194bcc04c7921
Dhiraj Gurung <57953730+grgdhiraj@users.noreply.github.com>
2019-12-30 10:04:11 +0100

    4.3 Done

--- js-complete-course/04-dates/03-age-by-select/script.js
+++ js-complete-course/04-dates/03-age-by-select/script.js
@@ -9,8 +9,28 @@
 // NOTE: don't focus on the existing code structure for now.
 // You will have time to focus on it later.
 
+
 (function() {
-
     // your code here
+    document.getElementById("run").addEventListener('click', checkAge);
 
+    function checkAge() {
+        console.log("test");
+        let BirthDay = document.getElementById("dob-day").value;
+        let BirthMonth = document.getElementById("dob-month").value;
+        let BirthYear = document.getElementById("dob-year").value;
+        console.log(BirthDay+" "+ BirthMonth +" "+ BirthYear);
+
+
+        let date = new Date();
+        console.log(date);
+        let CurrentYear = date.getFullYear(); //gives present year
+        console.log(CurrentYear);
+
+       let calculate = CurrentYear - BirthYear;
+        console.log(calculate);
+        alert(calculate);
+
+    }
+    
 })();
