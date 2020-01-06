c0fe110c2f2e1a7c15c85adc95625ff29252f9e5
DHIRAJ_GURUNG <grgdhiraj.be@gmail.com>
2020-01-06 17:04:42 +0100

    Final commit of the day

--- js-complete-course/07-misc/01-waving-text/script.js
+++ js-complete-course/07-misc/01-waving-text/script.js
@@ -10,22 +10,22 @@
 // You will have time to focus on it later.
 
 (function() {
-    var container = document.getElementById("target");
-    var letters = document.getElementById("target").innerText;
+    let container = document.getElementById("target");
+    let letters = document.getElementById("target").innerText;
     console.log(letters);
     letters = letters.split("");
     console.log(letters);
     container.innerText = "";
-    var fsize = 20;
-    var checker = true;
+    let textSize = 20;
+    let checker = true;
     letters.forEach(function(letter) {
-        var wrap = document.createElement("span");
+        let wrap = document.createElement("span");
         wrap.innerText = letter;
-        wrap.style.fontSize = fsize +"px";
-        if (fsize > 50) {
-            fsize = 20;
+        wrap.style.fontSize = textSize +"px";
+        if (textSize > 50) {
+            textSize = 20;
         }
-        fsize += 5;
+        textSize += 5;
 
         container.appendChild(wrap);
     });
